abd9831ab2f67f0e95ba031fd773d01db418ce09
pak611 <pak611@utulsa.edu>
2022-12-15 10:00:09 -0600

    added static directory

--- iotd/iotd/settings.py
+++ iotd/iotd/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#=> WindowsPath('C:/Users/patri/Dropbox/Ph.D/OrganoNet/Websites/postgresql_aws2/updated_iotd/iotd')
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
